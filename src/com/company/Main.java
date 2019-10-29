acbc6fa4bfc4bd66daedb471e365e35031fcc601
max-lian <max-lian@users.noreply.github.com>
2019-10-29 16:22:16 +0200

    fix sum

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,8 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-	    double a[] ={1.f,2.f,3.f,2.f};
-        double b[] = {1.f,2.f,3.f,4.f,5.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f};
+	    double[] a ={1.f,2.f,3.f,2.f};
+        double[] b = {1.f,2.f,3.f,4.f,5.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f,0.f};
 	    ArrayVectorImpl vector1 = new ArrayVectorImpl();
         ArrayVectorImpl vector2 = new ArrayVectorImpl();
 	    vector1.set(a);
